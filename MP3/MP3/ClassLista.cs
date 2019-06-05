62454a29af57b311a74094fe4334f4e1e74e19c0
Axel Fabián Avilá García <latente2010@hotmail.com>
2019-06-05 11:01:04 -0600

    MP3

--- MP3/MP3/ClassLista.cs
+++ MP3/MP3/ClassLista.cs
@@ -8,6 +8,7 @@ namespace MP3
     class ClassLista
     {
         string nombreLista;
+        string direccion;
 
         public string NombreLista { get => nombreLista; set => nombreLista = value; }
     }
